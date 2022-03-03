9477ceebe1d0a05bab870b44ffc1a253f91d8ab7
truongmagic9a@gmail.com <truongmagic9a@gmail.com>
2022-03-03 17:56:33 +0700

    truong

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_all_fe/common/export_this.dart';
-import 'package:shop_all_fe/view/login_controller.dart';
+
+import 'login_controller.dart';
+
 
 class LoginPage extends BaseView<LoginController> {
   LoginPage({Key? key}) : super(key: key);
@@ -11,7 +13,51 @@ class LoginPage extends BaseView<LoginController> {
   @override
   Widget buildSuccess() {
     return Container(
-      child: Text('Thành công'),
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              'Đăng nhập thành công',
+              style: appStyle.textTheme.headline1,
+            ),
+            BaseElevatedButton(
+              onTab: () {},
+              title: 'Elevate Button',
+              borderRadiusValue: 8,
+            ),
+            BaseTextButton(
+              onTab: () {},
+              title: 'Text Button',
+            ),
+            BaseOutlinedButton(
+              onTab: () {},
+              title: 'Outlined Button',
+              borderRadiusValue: 30,
+            ),
+            BaseInkWell(
+              title: 'Click me',
+              borderRadiusValue: 10,
+              dottedDecoration:
+              DottedDecoration(
+              shape: Shape.box,
+              dash: <int>[1, 4],
+              borderRadius: BorderRadius.circular(10),
+              ),
+              onTab: () {},
+            ),
+
+            BaseImage.asserts(
+                width: 240,
+                height: 240,
+                path: ImageResource.abc,
+                borderRadiusValue: 8,
+                boxFit: BoxFit.cover
+            ),
+            const BaseIndicator(),
+
+          ],
+        ),
+      ),
     );
   }
 }
