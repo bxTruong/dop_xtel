b57d5ef2fbaed17ffb8f904f9a3ea2915efd842d
Kết <TrungKetLB97>
2022-03-22 20:28:25 +0700

    update base

---
+++ lib/view/login/login_controller.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'package:app_shopee_lite/common/core/base_controller.dart';
+
+class LoginController extends BaseController{
+
+  @override
+  Future initialData() async{
+        super.initialData();
+  }
+}
---
+++ lib/view/login/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:app_shopee_lite/view/login/login_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginPage extends GetView<LoginController>{
+
+  LoginPage({Key? key}) : super(key: key);
+  final _controller = Get.lazyPut(()=> LoginController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
